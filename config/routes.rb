0c3e601a4a721bc4f438bea0abdcffa417a54639
Alena Bierzynski <abier070@uottawa.ca>
2019-05-27 15:24:38 -0400

    rails blog complete

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+
+  root 'welcome#index'
+  resources :articles do
+    resources :comments
+  end
+end
